84614551ead58fa2d4bd02fe6c1e91720d566525
Martin Ebel <ebelm86@gmail.com>
2017-01-05 16:07:29 +0100

    added text resources migration

---
+++ DeviceReg/DeviceReg.Common.Data/Migrations/201701051505503_AddedTextResources.cs
@@ -0,0 +1,31 @@
+namespace DeviceReg.Common.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedTextResources : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.TextResources",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Lang = c.String(),
+                        Text = c.String(),
+                        Timestamp_Created = c.DateTime(),
+                        Timestamp_Deleted = c.DateTime(),
+                        Timestamp_Modified = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.TextResources");
+        }
+    }
+}
