7172c53735d25ff712a7dc68904689de516f6c73
ardalan jasbi <ardalan.jasbi84@gmail.com>
2017-01-15 15:10:16 +0100

    DeviceReg.Integration.ApiTest

---
+++ DeviceReg/DeviceReg.Integration.ApiTest/AbstractApiTest.cs
@@ -0,0 +1,35 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceReg.Integration.ApiTest
+{
+    public class AbstractApiTest
+    {
+        protected string ServerUrl { get { return "https://www.devRegProject.com"; } }
+
+        RestClient _RestClient;
+        protected RestClient RestClient { get { return _RestClient; } }
+
+        public AbstractApiTest()
+        {
+            _RestClient = new RestClient(ServerUrl);
+        }
+
+
+        protected RestRequest GetRequest(string resource)
+        {
+            var request = new RestRequest(resource);
+
+            request.Method = Method.GET;
+            request.AddHeader("Content-Type", "application/json");
+            request.Parameters.Clear();
+            request.RequestFormat = DataFormat.Json;
+
+            return request;
+        }
+    }
+}
---
+++ DeviceReg/DeviceReg.Integration.ApiTest/DeviceApiTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceReg.Integration.ApiTest
+{
+    [TestFixture]
+    public class DeviceApiTest:AbstractApiTest
+    {
+        [Test]
+        public void TestGetDeviceById()
+        {
+            
+            var request = GetRequest("/Device/GetDeviceById");
+
+            //TODO : you should add paramter for request
+            //request.AddBody();
+
+            var response = RestClient.Execute(request);
+
+            //check response and 
+            Assert.AreEqual("", response);
+        }
+    }
+}
